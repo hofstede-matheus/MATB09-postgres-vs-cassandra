d293e83b18f96d8552a75edc21bbcb56b3ef8178
hofstede-matheus <hofstede.matheus@gmail.com>
2022-11-08 10:54:18 -0300

    configure cassandra

--- initdb.sql
+++ initdb.sql
@@ -4,81 +4,76 @@ create extension if not exists "uuid-ossp";
 
 -- Tables --
 
--- Colegiado
-CREATE TABLE Colegiado
-(
-codigo varchar(20) NOT NULL ,
-nome   varchar(20) NOT NULL ,
+  -- Colegiado
+  CREATE TABLE Colegiado
+  (
+  codigo varchar(20) NOT NULL ,
+  nome   varchar(20) NOT NULL ,
 
-PRIMARY KEY (codigo)
-);
+  PRIMARY KEY (codigo)
+  );
 
--- Aluno
-CREATE TABLE Aluno
-(
-matricula       varchar(20) NOT NULL ,
-email           varchar(20) NOT NULL ,
-idColegiado     varchar(20) NOT NULL ,
-senha           varchar(20) NOT NULL ,
-numero_telefone varchar(20) NULL ,
+  -- Aluno
+  CREATE TABLE Aluno
+  (
+  matricula       varchar(20) NOT NULL ,
+  email           varchar(20) NOT NULL ,
+  idColegiado     varchar(20) NOT NULL ,
+  senha           varchar(20) NOT NULL ,
+  numero_telefone varchar(20) NULL ,
 
-PRIMARY KEY (matricula),
-FOREIGN KEY (idColegiado) REFERENCES Colegiado (codigo)
-);
+  PRIMARY KEY (matricula),
+  FOREIGN KEY (idColegiado) REFERENCES Colegiado (codigo)
+  );
 
--- Coordenador
-CREATE TABLE Coordenador
-(
-matricula   varchar(20) NOT NULL ,
-email       varchar(20) NOT NULL ,
-idColegiado varchar(20) NOT NULL ,
-senha       varchar(20) NOT NULL ,
+  -- Coordenador
+  CREATE TABLE Coordenador
+  (
+  matricula   varchar(20) NOT NULL ,
+  email       varchar(20) NOT NULL ,
+  idColegiado varchar(20) NOT NULL ,
+  senha       varchar(20) NOT NULL ,
 
-PRIMARY KEY (matricula),
-FOREIGN KEY (idColegiado) REFERENCES Colegiado (codigo)
-);
+  PRIMARY KEY (matricula),
+  FOREIGN KEY (idColegiado) REFERENCES Colegiado (codigo)
+  );
 
--- Fila
-CREATE TABLE Fila
-(
-id   int NOT NULL ,
-nome int NOT NULL ,
+  -- Fila
+  CREATE TABLE Fila
+  (
+  id   int NOT NULL ,
+  nome int NOT NULL ,
 
-PRIMARY KEY (id)
-);
+  PRIMARY KEY (id)
+  );
 
--- Telao
-CREATE TABLE Telao
-(
-id     int NOT NULL ,
-titulo int NOT NULL ,
+  -- Telao
+  CREATE TABLE Telao
+  (
+  id     int NOT NULL ,
+  titulo int NOT NULL ,
 
-PRIMARY KEY (id)
-);
+  PRIMARY KEY (id)
+  );
 
--- AlunoFila
-CREATE TABLE AlunoFila
-(
-alunoMatricula varchar(20) NOT NULL ,
-filaId         int NOT NULL ,
+  -- AlunoFila
+  CREATE TABLE AlunoFila
+  (
+  alunoMatricula varchar(20) NOT NULL ,
+  filaId         int NOT NULL ,
 
-FOREIGN KEY (alunoMatricula) REFERENCES Aluno (matricula),
-FOREIGN KEY (filaId) REFERENCES Fila (id)
-);
+  FOREIGN KEY (alunoMatricula) REFERENCES Aluno (matricula),
+  FOREIGN KEY (filaId) REFERENCES Fila (id)
+  );
 
--- filaTelao
-CREATE TABLE filaTelao
-(
-filaId  int NOT NULL ,
-telaoId int NOT NULL ,
+  -- filaTelao
+  CREATE TABLE filaTelao
+  (
+  filaId  int NOT NULL ,
+  telaoId int NOT NULL ,
 
-FOREIGN KEY (filaId) REFERENCES Fila (id),
-FOREIGN KEY (telaoId) REFERENCES Telao (id)
-);
-
-
-
-
-
+  FOREIGN KEY (filaId) REFERENCES Fila (id),
+  FOREIGN KEY (telaoId) REFERENCES Telao (id)
+  );
 
 -- Seeds
